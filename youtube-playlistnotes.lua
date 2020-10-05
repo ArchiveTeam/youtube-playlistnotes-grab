8fb82f9c412d62f030aa1b8b473bfe027c2b0b1d
arkiver <arkiver@protonmail.com>
2020-10-05 12:17:59 +0200

    Version 20201005.02. Do not write bad responses to WARC.

--- youtube-playlistnotes.lua
+++ youtube-playlistnotes.lua
@@ -243,7 +243,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
 end
 
 wget.callbacks.write_to_warc = function(url, http_stat)
-  if http_stat["statcode"] ~= 200 and http_stat["statcode"] ~= 302 then
+  if http_stat["statcode"] ~= 200 and http_stat["statcode"] ~= 404 then
     return false
   end
   return true
